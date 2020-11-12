c9d7ef3fef51c62ecf1bb0406f85655b5d1986f3
Music <musicm987@gmail.com>
2020-11-12 14:10:46 -0700

    added post method for quotes

--- migrations/20201109150639_create-categories-table.js
+++ migrations/20201109150639_create-categories-table.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable("categories", table => {
-        table.integer("id")
+        table.increments("id")
         table.string("title")
         table.string("url")
     })
 };
 
 exports.down = function(knex) {
-    return knex.scheme.dropTableIfExists("categories")
+    return knex.schema.dropTableIfExists("categories")
 };
--- seeds/01-create-categories.js
+++ seeds/01-create-categories.js
@@ -2,12 +2,25 @@
 exports.seed = async knex => {
   await knex('categories').del()
   await knex('categories').insert([{
-    id: 1,
     title: "Inspirational",
     url: "https://www.incimages.com/uploaded_files/image/1920x1080/getty_615524918_379849.jpg"
   },{
-    id: 2,
     title: "Motivational",
     url: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F982740466%2F960x0.jpg%3Ffit%3Dscale"
+  },{
+    title: "Life",
+    url: "https://www.incimages.com/uploaded_files/image/1920x1080/getty_153081592_970647970450075_79973.jpg"
+  },{
+    title: "Funny",
+    url: "https://images.pexels.com/photos/1183434/pexels-photo-1183434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },{
+    title: "Friendship",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTUyq274IZsNFB6s0NePECOO1NqwRnKDJ346g&usqp=CAU"
+  },{
+    title: "Love",
+    url: "https://media.swncdn.com/cms/BST/67912-gettyimages-817147678-kieferpix.1200w.tn.jpg"
+  },{
+    title: "Success",
+    url: "https://cdn.lifehack.org/wp-content/uploads/2020/01/road-to-success.jpeg"
   }])
 };
